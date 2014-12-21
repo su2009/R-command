83f16fc1d01f005838117bef863f5e0442558b8f
su2009 <yangsu2009@gmail.com>
2014-12-21 11:53:01 -0500

    Create Manipulate data.R

---
+++ Manipulate data.R
@@ -0,0 +1,20 @@
+# Create variable, vector and matrix
+
+
+# summarize existing objective
+
+
+# deal with missing value & NA value
+
+
+# add new row/column to an exsiting objective
+
+
+# subset certain parts of an existing objective
+
+
+# merge two objective
+
+
+# identify variables or records matching certain criteria 
+
